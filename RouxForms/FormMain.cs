6445ff81b1766c24ab032fd7f500cdcdb3f699c0
BradDesktop <brad.martin@xdisplay.com>
2022-01-18 11:59:52 -0500

    simplify bmp load

--- RouxForms/FormMain.cs
+++ RouxForms/FormMain.cs
@@ -11,10 +11,8 @@ namespace RouxForms
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            Bitmap bmp = new(Properties.Resources.car);
-            if (radioButton2.Checked) bmp = new Bitmap(Properties.Resources.cube);
-            Bitmap resized = new(bmp, new Size(bmp.Width / 5, bmp.Height / 5));
-            label1.Text = GetEntropy(GetRedChannelArr(ref resized)).ToString();
+            Bitmap bmp = radioButton1.Checked ? Properties.Resources.car : Properties.Resources.cube;
+            label1.Text = GetEntropy(GetRedChannelArr(ref bmp)).ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
